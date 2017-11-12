773fca4365ab48b3ab223d1545fa37b62744e8ff
Karl Ranna <karl@karlranna.com>
2017-11-12 11:28:04 +0200

    Create app shell test

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,6 +6,17 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
+    'ember-app-shell': {
+      visitPath: '/app-shell',
+      outputFile: 'index.html',
+      // https://peter.sh/experiments/chromium-command-line-switches/
+      chromeFlags: [],
+      // https://github.com/addyosmani/critical#options
+      criticalCSSOptions: {
+        width: 1300,
+        height: 900
+      }
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
